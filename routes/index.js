aa573c73b14ec035ed2f00959fa9b35715633bb9
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-09-05 14:31:50 -0400

    Add routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/sign-up", (req, res) => res.render("sign-up-form"));
+router.post("/sign-up", (req, res) => {
+  console.log("We're here");
+  res.redirect("/");
 });
 
 module.exports = router;
