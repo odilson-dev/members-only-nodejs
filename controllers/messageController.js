65dc9474830ea90a2e24588cd2c112d001e95382
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-09-09 17:05:31 -0400

    Create message controller

---
+++ controllers/messageController.js
@@ -0,0 +1,27 @@
+const db = require("../db/queries");
+const asyncHandler = require("express-async-handler");
+const { matchedData, validationResult } = require("express-validator");
+
+// Create a new user
+const createMessage = asyncHandler(async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const data = matchedData(req);
+
+  try {
+    await db.insertMessage(data);
+
+    // Send a success response
+    res.status(201).json(data);
+  } catch (error) {
+    // Handle errors from bcrypt or any other issue
+    res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
+  }
+});
+module.exports = { createMessage };
