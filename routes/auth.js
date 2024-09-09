ca380bb7a211aa4febb098c2c9de7d769db4e614
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-09-08 21:53:44 -0400

    Use local strategy to authenticate uers via passportjs

--- routes/auth.js
+++ routes/auth.js
@@ -4,4 +4,54 @@ const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 
+passport.use(
+  new LocalStrategy(async (email, password, done) => {
+    try {
+      const { rows } = await pool.query(
+        "SELECT * FROM users WHERE email = $1",
+        [email]
+      );
+      const user = rows[0];
+
+      if (!user) {
+        return done(null, false, { message: "Incorrect email" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  })
+);
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+router.get("/", (req, res) => {
+  res.render("index", { user: req.user });
+});
+
+router.post(
+  "/log-in",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/",
+  })
+);
+
 module.exports = router;
