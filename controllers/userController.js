aeeb5c7c63076ce5dfc94065c2c6146bc85b3d29
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-09-08 21:38:54 -0400

    Create authROuter

--- controllers/userController.js
+++ controllers/userController.js
@@ -37,7 +37,8 @@ const checkPassCode = asyncHandler(async (req, res) => {
   if (errors.isEmpty()) {
     const { id } = req.params;
     await db.updateMembershipStatus(id, true);
-    return res.send("Success, you're a member");
+    return r
+    res.send("Success, you're a member");
   }
   res.status(400).json({ errors: errors.array() });
 });
---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+var express = require("express");
+var router = express.Router();
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+
+module.exports = router;
