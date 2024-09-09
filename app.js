d12c4471b66025b46a88c0a64f0197f91e876069
odilsonjs <italisodilsonwoodly@hotmail.com>
2024-09-09 17:07:40 -0400

    Use message routes

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const passport = require("passport");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
+var messageRouter = require("./routes/message");
 
 var app = express();
 
@@ -30,6 +31,7 @@ app.use(passport.session());
 app.use("/", indexRouter);
 app.use("/", authRouter);
 app.use("/users", usersRouter);
+app.use("/messages", messageRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
